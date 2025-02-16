221b1b2fe332b01270c3c36303eb70059a0c1f8a
KB <kmbhm1@gmail.com>
2025-02-16 10:48:23 -0600

    fix: line length error

--- supabase_pydantic/util/writers/sqlalchemy_writers.py
+++ supabase_pydantic/util/writers/sqlalchemy_writers.py
@@ -115,7 +115,7 @@ class SqlAlchemyFastAPIClassWriter(AbstractClassWriter):
             back_ref = pluralize(self.table.name.lower())
             if fk.relation_type == RelationType.ONE_TO_ONE:
                 type_hint = f'Mapped[{target_class} | None]'
-                rel_str = f'{rel_name}: {type_hint} = relationship("{target_class}", back_populates="{back_ref}", uselist=False)'
+                rel_str = f'{rel_name}: {type_hint} = relationship("{target_class}", back_populates="{back_ref}", uselist=False)'  # noqa: E501
             else:  # ONE_TO_MANY or MANY_TO_MANY
                 rel_name = pluralize(rel_name)
                 type_hint = f'Mapped[list[{target_class}]]'

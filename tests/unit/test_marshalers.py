660895ef5ac5e9e546f212797d0bbd027f0e4378
KB <kmbhm1@gmail.com>
2025-07-30 05:26:33 -0500

    feat: add 'credits' as reserved column name exception

--- tests/unit/test_marshalers.py
+++ tests/unit/test_marshalers.py
@@ -317,6 +317,7 @@ def test_column_name_reserved_exceptions(column_name, expected):
         ('model_abc', 'field_model_abc'),  # Starts with 'model_'
         ('username', 'username'),  # Not a reserved name
         ('id', 'id'),  # Exception, should not be modified
+        ('credits', 'credits'),  # Business term exception, should not be modified
     ],
 )
 def test_standardize_column_name(column_name, expected):
